80303bb1ae4c419152878be0b6322fd0b1213f08
Ihza Oktavira <57393641+ihza149@users.noreply.github.com>
2020-06-25 21:51:39 +0700

    Beranda

--- MobileAndroid/KostKita_Project_4/volley/app/src/main/java/com/kos/KostKita/Beranda.java
+++ MobileAndroid/KostKita_Project_4/volley/app/src/main/java/com/kos/KostKita/Beranda.java
@@ -2,13 +2,52 @@ package com.kos.KostKita;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Beranda extends AppCompatActivity {
+
+    Button Mprofil, Mdata, Mtipe, Mlogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beranda);
+
+        Mprofil = (Button)findViewById(R.id.tmblProfil);
+        Mdata = (Button)findViewById(R.id.tmblDataKos);
+        Mtipe = (Button)findViewById(R.id.tmblTipeKamar);
+        Mlogout = (Button)findViewById(R.id.tmblLogout);
+
+        Mprofil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent profil = new Intent(getApplicationContext(),ProfileActivity.class);
+                startActivity(profil);
+                finish();
+            }
+        });
+
+        Mdata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent(getApplicationContext(),DetailDataKos.class);
+                startActivity(data);
+                finish();
+            }
+        });
+
+        Mtipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent tipe = new Intent(getApplicationContext(),TipeKamar.class);
+                startActivity(tipe);
+                finish();
+            }
+        });
+
+
     }
 }
